d65fb073fdb7f0b218fe8dee769988d1c9862c00
Joslyn <jrosbrook@gmail.com>
2014-03-10 10:44:43 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Joslynrosbrookdotcom::Application.config.secret_key_base = '314883213c3bd5b5885d4160c79b055d9091a940dbe7b61a33595abef8307d53d80babf0721cd37c8ce06f0714787129b52ae6c63fce17ae40d9bf72da35fa60'
