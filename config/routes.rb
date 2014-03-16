3b8d4a7bd59f7290e84800aee805b5e95e2e7119
Joslyn <jrosbrook@gmail.com>
2014-03-15 23:16:22 -0700

    skeleton of static portfolio views

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Joslynrosbrookdotcom::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#index'
+  root 'static_pages#projects'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
